2389c41b67025565c1a15e85a5d5791e3ff0a862
CodeAFox <150443592+CodeAFox@users.noreply.github.com>
2026-04-30 20:42:17 +0200

    Added minor changes
    Wanted to add a nav mesh to enable the slime's movement, however that did not work out.

--- Assets/Scripts/SlimeController.cs
+++ Assets/Scripts/SlimeController.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class SlimeController : MonoBehaviour
 {
+    public Transform player;
+    //private NavMeshAgent navMeshAgent;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //navMeshAgent.SetDestination(-player.position);
     }
 }
