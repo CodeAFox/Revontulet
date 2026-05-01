860ef33be7846f4cd61afde234eefb3349b330ff
CodeAFox <150443592+CodeAFox@users.noreply.github.com>
2026-05-01 19:33:11 +0200

    Added movement
    Added random movement to the slime

--- Assets/Scripts/SlimeController.cs
+++ Assets/Scripts/SlimeController.cs
@@ -1,22 +1,41 @@
+using System.Threading;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class SlimeController : MonoBehaviour
 {
     public Transform player;
+    public float speed = 1;
     private Rigidbody2D slimeRB;
+    private Vector2 movement;
+    private float timer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         slimeRB = GetComponent<Rigidbody2D>();
         slimeRB.MovePosition(SpawnAwayFromPlayer(player, 3));
+
+        movement = Random.insideUnitCircle.normalized;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        timer -= Time.deltaTime;
         
+        slimeRB.MovePosition(slimeRB.position + movement * speed * Time.fixedDeltaTime);
+
+        if(timer <= 0)
+        {
+            ChangeMovement();
+        }
+    }
+
+    private void ChangeMovement()
+    {
+        movement = Random.insideUnitCircle.normalized;
+        timer = 5;
     }
 
     // Should be moved to a parent class to be used by other enemies too
