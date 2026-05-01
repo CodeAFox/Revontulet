64dc7c0b5d47f47c64278f5b32a2a33e523badd1
CodeAFox <150443592+CodeAFox@users.noreply.github.com>
2026-05-01 19:04:32 +0200

    Spawn mechanics
    Added fixed spawn mechanics for the slime game object

--- Assets/Scripts/SlimeController.cs
+++ Assets/Scripts/SlimeController.cs
@@ -4,17 +4,25 @@ using UnityEngine.AI;
 public class SlimeController : MonoBehaviour
 {
     public Transform player;
-    //private NavMeshAgent navMeshAgent;
+    private Rigidbody2D slimeRB;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //navMeshAgent = GetComponent<NavMeshAgent>();
+        slimeRB = GetComponent<Rigidbody2D>();
+        slimeRB.MovePosition(SpawnAwayFromPlayer(player, 3));
     }
 
     // Update is called once per frame
     void Update()
     {
-        //navMeshAgent.SetDestination(-player.position);
+        
+    }
+
+    // Should be moved to a parent class to be used by other enemies too
+    private Vector2 SpawnAwayFromPlayer(Transform player, int magnitude)
+    {
+        Vector2 randVector = Random.insideUnitCircle.normalized * magnitude;
+        return new Vector2(player.position.x + randVector.x, player.position.y + randVector.y);
     }
 }
