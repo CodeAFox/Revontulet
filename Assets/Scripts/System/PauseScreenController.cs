628dfb63eb4d1518e80e0336a6a4c30d2f770261
CodeAFox <150443592+CodeAFox@users.noreply.github.com>
2026-05-11 04:12:03 +0200

    Pause menu functional
    Added pause menu with more or less functional buttons

---
+++ Assets/Scripts/System/PauseScreenController.cs
@@ -0,0 +1,60 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PauseScreenController : MonoBehaviour
+{
+    public static bool paused = false;
+    public GameObject pauseScreen;
+
+    public GameObject resumeButton;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Cancel"))
+        {
+            if(paused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    private void Pause()
+    {
+        pauseScreen.SetActive(true);
+        Time.timeScale = 0f;
+        paused = true;
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(resumeButton);
+    }
+
+    public void Resume()
+    {
+        pauseScreen.SetActive(false);
+        Time.timeScale = 1f;
+        paused = false;
+    }
+
+    public void EnterSettings()
+    {
+        //todo
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
