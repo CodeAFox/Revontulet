88a4d12cdf317310879635411de58441b58df6fd
CodeAFox <150443592+CodeAFox@users.noreply.github.com>
2026-05-21 02:04:30 +0200

    Movement sound to player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -10,16 +9,17 @@ public class PlayerController : MonoBehaviour
     private float movementY;
     private MovementAnimator animationLogic;
     private AudioSource audioSource;
+    private Vector2 position;
 
     public Animator anim;
     public float speed = 0;
-    public List<AudioClip> audios;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         player = GetComponent<Rigidbody2D>();
         audioSource = GetComponent<AudioSource>();
+        position = transform.position;
         
         transform.GetComponent<Rigidbody2D>().freezeRotation = true;
         animationLogic = new MovementAnimator(anim, player.gameObject);
@@ -32,10 +32,17 @@ public class PlayerController : MonoBehaviour
             player.position + 
             new Vector2(movementX, movementY) * 
             speed * Time.fixedDeltaTime);
+
+        if(Vector2.Distance(transform.position, position) > 2)
+        {
+            audioSource.Play();
+            position = transform.position;
+        }
     }
 
     void OnMove(InputValue movementValue)
     {
+        audioSource.Play();
         Vector2 movementVector = movementValue.Get<Vector2>();
 
         movementX = movementVector.x;
